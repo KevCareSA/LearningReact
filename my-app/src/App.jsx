4fa3282dac805eca7d1040737b52384d6cbc233c
KevCareSA <kev.mkbn2@gmail.com>
2024-08-27 23:59:03 +0200

    my-app structure

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+// import Message from "./components/Message";
+import StateDemo from "./components/StateDemo";
+
+export default function App() {
+
+  return (
+    <div className="App">
+     
+      {/* <Message /> */}
+      <StateDemo/>
+    </div>
+  );
+  
+}
