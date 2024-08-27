4af22d27fe719c63765a5a17e8e585be6adc80cd
KevCareSA <kev.mkbn2@gmail.com>
2024-08-27 23:58:01 +0200

    React Practice: Message component

---
+++ my-app/src/components/Message.jsx
@@ -0,0 +1,15 @@
+
+// export default function App() {
+
+//     function handleClick() {
+//         console.log("Button Clicked")
+
+//     }
+//     return (
+//         <div>
+//             <button onClick={handleClick}>
+//                 <h2>Click To Get Message</h2>
+//             </button>
+//         </div>
+//     )
+// }
